ab0669b6a0e8c5b0324202d2fa92ada09ca7820d
kolpakovee <90896437+kolpakovee@users.noreply.github.com>
2021-11-03 10:13:34 +0300

    Дз + семинары

---
+++ 02module/02seminar/02_01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _02_01
+{
+    class MyComplex
+    {
+        public double re { get; set; }
+        public double im { get; set; }
+        public MyComplex(double xre, double xim)
+        { re = xre; im = xim; }
+        public static MyComplex operator ++(MyComplex mc)
+        { mc.re++; mc.im++; return new MyComplex(mc.re + 1, mc.im + 1); }
+        public static MyComplex operator --(MyComplex mc)
+        { return new MyComplex(mc.re - 1, mc.im - 1); }
+        public double Mod() { return Math.Abs(re * re + im * im); }
+        static public bool operator true(MyComplex f)
+        {
+            if (f.Mod() > 1.0) return true;
+            return false;
+        }
+        static public bool operator false(MyComplex f)
+        {
+            if (f.Mod() <= 1.0) return true;
+            return false;
+        }
+        public static MyComplex operator +(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re + b.re, a.im + b.im);
+        }
+        public static MyComplex operator -(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re - b.re, a.im - b.im);
+        }
+        public static MyComplex operator *(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re*b.re - a.im * b.im, a.re*b.im + b.re* a.im);
+        }
+        public static MyComplex operator /(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re * b.re + a.im * b.im / b.re * b.re + b.im * b.im, a.im * b.re - a.re * b.im / b.re * b.re + b.im * b.im);
+        }
+        public override string ToString()
+        {
+            return re + " " + im;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplex a = new MyComplex(3, 2);
+            MyComplex b = new MyComplex(4, 5);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a++);
+        }
+    }
+}
---
+++ 02module/02seminar/02_02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _02_02
+{
+    class ConsolePlate
+    {
+        char _plateChar;
+        ConsoleColor _plateColor;
+        ConsoleColor _consoleColor;
+        public ConsolePlate()
+        {
+            _plateChar = 'A';
+        }
+        public ConsolePlate(char value, ConsoleColor color, ConsoleColor color2)
+        {
+            _plateChar = value;
+            _plateColor = color;
+            _consoleColor = color2;
+
+        }
+        public char PlateChar
+        {
+            set
+            {
+                if (value >= 41 && value <= 66)
+                    _plateChar = value;
+                else
+                    _plateChar = 'A';
+            }
+            get { return _plateChar; }
+        }
+        public ConsoleColor PlateColor
+        {
+            set { _plateColor = value; }
+            get { return _plateColor; }
+        }
+        public ConsoleColor Con_Color
+        {
+            set
+            {
+                if (value != _plateColor)
+                    _consoleColor = value;
+                else if (value != ConsoleColor.DarkMagenta)
+                    _consoleColor = ConsoleColor.DarkMagenta;
+                else
+                    _consoleColor = ConsoleColor.DarkBlue;
+            }
+            get { return _consoleColor; }
+        }
+        public void Print(ConsolePlate cp)
+        {
+            char c = cp.PlateChar;
+            Console.ForegroundColor = cp.PlateColor;
+            Console.BackgroundColor = cp.Con_Color;
+            Console.Write(c);
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsolePlate first = new ConsolePlate('X', ConsoleColor.White, ConsoleColor.Red);
+            ConsolePlate second = new ConsolePlate('O', ConsoleColor.White, ConsoleColor.Magenta);
+            ConsolePlate[] arr = new ConsolePlate[] { first, second };
+            int n;
+            do
+                Console.Write("Введите N: ");
+            while (!(int.TryParse(Console.ReadLine(), out n) && n >= 2 && n  < 35));
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == n - 1)
+                    {
+                        first.Print(arr[new Random().Next(0, arr.Length)]);
+                        Console.WriteLine();
+                    }
+                    else
+                        first.Print(arr[new Random().Next(0, arr.Length)]);
+                }
+
+        }
+    }
+}
