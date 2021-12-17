782f854c08c84669b0edd0de1206be992863083e
kolpakovee <90896437+kolpakovee@users.noreply.github.com>
2021-12-17 19:54:33 +0300

    HW10

---
+++ 02module/10seminar/10_01/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_01
+{
+    public partial class Form1 : Form
+    {
+        int X, Y;
+        int W, H;
+
+        public Form1()
+        {
+            InitializeComponent();
+            X = Y = 0;
+            W = H = 100;
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            trackBar1.Maximum = Width - W;
+            e.Graphics.FillRectangle(new SolidBrush(SystemColors.ControlDark), X, Y, W, H);
+            TransparencyKey = SystemColors.ControlDark;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            X = trackBar1.Value;
+            Invalidate();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Y = trackBar2.Value;
+            Invalidate();
+        }
+    }
+}
